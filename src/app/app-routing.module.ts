580ff5185e568cf8577e1667d42838e5f595292f
Marcelo <marcelozapatta0@gmail.com>
2020-04-26 15:46:52 -0300

    Adicionando AuthService e AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'places', pathMatch: 'full' },
@@ -10,12 +11,14 @@ const routes: Routes = [
   {
     path: 'places',
     loadChildren: () =>
-      import('./places/places.module').then(m => m.PlacesPageModule)
+      import('./places/places.module').then(m => m.PlacesPageModule),
+    canLoad: [AuthGuard]
   },
   {
     path: 'bookings',
     loadChildren: () =>
-      import('./bookings/bookings.module').then(m => m.BookingsPageModule)
+      import('./bookings/bookings.module').then(m => m.BookingsPageModule),
+    canLoad: [AuthGuard]
   }
 ];
 
