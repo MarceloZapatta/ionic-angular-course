226b0e427787392e158f23fc40bb0fb367b0e092
Marcelo <marcelozapatta0@gmail.com>
2020-07-15 20:56:50 -0300

    Controlling grid alignment

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -14,7 +14,7 @@ export class AuthGuard implements CanLoad {
     segments: UrlSegment[]
   ): Observable<boolean> | Promise<boolean> | boolean {
     if (!this.authService.userIsAuthenticated) {
-      this.router.navigateByUrl('/auth');
+      // this.router.navigateByUrl('/auth');
     }
 
     return true;
