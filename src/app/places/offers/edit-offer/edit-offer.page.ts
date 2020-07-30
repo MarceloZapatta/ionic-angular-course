876f16d279dc435afaa597b5004feb1ac9788b7a
Marcelo <marcelozapatta0@gmail.com>
2020-07-30 19:58:49 -0300

    Reative form na tela de edição Oferta

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -3,6 +3,7 @@ import { Place } from '../../places.model';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { PlacesService } from '../../places.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-offer',
@@ -11,11 +12,16 @@ import { PlacesService } from '../../places.service';
 })
 export class EditOfferPage implements OnInit {
   place: Place;
+  form: FormGroup;
 
-  constructor(private activatedRoute: ActivatedRoute, private navController: NavController, private placesService: PlacesService) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private navController: NavController,
+    private placesService: PlacesService
+  ) {}
 
   ngOnInit() {
-    this.activatedRoute.paramMap.subscribe(paramMap => {
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
       if (!paramMap.has('placeId')) {
         this.navController.navigateBack('/places/tabs/offers');
         return;
@@ -23,10 +29,29 @@ export class EditOfferPage implements OnInit {
 
       this.place = this.placesService.getPlace(paramMap.get('placeId'));
     });
+    console.log(this.place);
+    this.form = new FormGroup({
+      title: new FormControl(this.place.title, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(this.place.description, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      price: new FormControl(this.place.price, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      imageUrl: new FormControl(this.place.imageUrl, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+    });
   }
 
   save() {
     console.log('Fui salvo!');
+    console.log(this.form.value);
   }
-
 }
